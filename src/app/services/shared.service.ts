7bb715473fdcb8fba84a688138c81bbcd75910b1
Manveer Chhabra <manveerchhabra56@gmail.com>
2020-05-31 02:52:20 +0530

    Home page with search feature for movies and tv shows.

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  baseURL = 'https://api.themoviedb.org/3/';
+  apiKey = '6bf8f692e90be1e321451eec713f8bc6';
+  constructor(private httpService: HttpClient) { }
+
+  getFilteredMovieResults(searchQuery, page): Observable<any>{
+    return this.httpService.get(`${this.baseURL}search/movie?query=${searchQuery}&api_key=${this.apiKey}&page=${page}`);
+  }
+
+  getFilteredTvShowResults(searchQuery, page): Observable<any>{
+    return this.httpService.get(`${this.baseURL}search/tv?query=${searchQuery}&api_key=${this.apiKey}&page=${page}`);
+  }
+}
